a12eacd48188183ff59d114c688f2bbd908f04e9
Danyil Fiut <danyilfiut@gmail.com>
2024-05-07 20:13:02 +0200

    project done

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const recipeController = require('../controllers/recipeController');
 
 // Define routes
+router.get('/', (req, res) => {
+    res.render('index');
+});
+
 router.get('/recipes', recipeController.getRecipes);
 router.post('/recipes', recipeController.addRecipe);
 router.get('/recipes/:index/edit', recipeController.getEditForm);
