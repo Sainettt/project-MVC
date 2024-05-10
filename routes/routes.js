99fa7a11e78c113aff65605c7edb01e7d3e4942c
Danyil Fiut <danyilfiut@gmail.com>
2024-05-10 21:22:14 +0200

    final

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const recipeController = require('../controllers/recipeController');
 
-// Define routes
 router.get('/', (req, res) => {
     res.render('index');
 });
