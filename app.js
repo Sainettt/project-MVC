dbe24eb40dc45266f4ece8e17290ffa7752a0664
Danyil Fiut <danyilfiut@gmail.com>
2024-05-14 23:11:33 +0200

    delete and css

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const routes = require('./routes/routes');
 
 const app = express();
+app.use('/css', express.static(path.join(__dirname, '/css')));
+
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
